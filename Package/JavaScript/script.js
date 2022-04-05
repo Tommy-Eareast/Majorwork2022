5e41ffdde3223d6bfe75b39c3d42c07a4570be98
TommyEareast <thomasyutao2004@gmail.com>
2022-04-05 20:04:10 +1000

    React introduced
    React library of JavaScript programming language used, learning from online resources and begin to integrate own project with React to make interactive user interfaces.

--- Package/JavaScript/script.js
+++ Package/JavaScript/script.js
@@ -1,9 +1,24 @@
-let time = new Date();
-let utc = {
-    year: time.getUTCFullYear(),
-    mon: time.getUTCMonth() + 1,
-    date: time.getUTCDate(),
-    hour: time.getUTCHours(),
-    min: time.getUTCMinutes(),
-    sec: time.getUTCSeconds()
-};
+while (true) {
+  let n = Date.now();
+}
+let utcTime = new Date(n)
+let offset = utcTime.getTimezoneOffset();
+let localTime = new Date(Date.now(n) + offset * 60000);
+let utcNow = {
+  year: utcTime.getFullYear(),
+  month: utcTime.getMonth() + 1,
+  date: utcTime.getDate(),
+  hour: utcTime.getHours(),
+  min: utcTime.getMinutes(),
+  sec: utcTime.getSeconds()
+}
+let localNow = {
+  year: localTime.getFullYear(),
+  month: localTime.getMonth() + 1,
+  date: localTime.getDate(),
+  hour: localTime.getHours(),
+  min: localTime.getMinutes(),
+  sec: localTime.getSeconds()
+}
+console.log(localNow);
+console.log(utcNow);
---
+++ PackageReact/majorwork/src/components/clock.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Clock extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <button>Change time format</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Clock;
