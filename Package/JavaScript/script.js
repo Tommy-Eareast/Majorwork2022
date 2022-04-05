9f1a599e014debd535e983bd8a3d62f8241f0b5d
TommyEareast <51252037+TommyAntien@users.noreply.github.com>
2022-04-05 13:29:59 +1000

    Update script.js

--- Package/JavaScript/script.js
+++ Package/JavaScript/script.js
@@ -1,6 +1,9 @@
-let today = {
-  date: 
-  month:
-  year: 
-}
-let date = {}
+let time = new Date();
+let utc = {
+    year: time.getUTCFullYear(),
+    mon: time.getUTCMonth() + 1,
+    date: time.getUTCDate(),
+    hour: time.getUTCHours(),
+    min: time.getUTCMinutes(),
+    sec: time.getUTCSeconds()
+};
