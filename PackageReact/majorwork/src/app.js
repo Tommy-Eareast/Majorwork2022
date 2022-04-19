21a31add7c99abcc32078e067a7dd9f58156ff97
TommyEareast <51252037+EareastTommy@users.noreply.github.com>
2022-04-19 17:52:52 +1000

    Switching component (Clock/Timer) without separate webpage achieved
    getting inspiration from previous useState expression, successfully complete the switching between Clock and Timer without the need to introduce a new html, Clock function fully completed (UI appearance and style still require optimisation)

--- PackageReact/majorwork/src/app.js
+++ PackageReact/majorwork/src/app.js
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.css";
 import Clock from "./components/clock";
 import Timer from "./components/timer";
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    {mode === "isClock" && <Clock />}
-    {mode === "isTimer" && <Timer />}
-  </React.StrictMode>,
-  document.getElementById("box")
-);
+//true for Clock, false for Timer
+export default function App() {
+  const [mode, setMode] = useState(true);
+
+  return (
+    <>
+      <nav>
+        <div className="row w-100">
+          <button onClick={() => setMode(true)} className="btn col col-6">
+            Clock
+          </button>
+          <button onClick={() => setMode(false)} className="btn col col-6">
+            Timer
+          </button>
+        </div>
+      </nav>
+      <div>
+        {mode && <Clock />}
+        {!mode && <Timer />}
+      </div>
+    </>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- PackageReact/majorwork/src/index.js
+++ PackageReact/majorwork/src/index.js
@@ -2,35 +2,12 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.css";
-import Clock from "./components/clock";
-import Timer from "./components/timer";
-
-const header = (
-  <header>
-    <nav>
-      <div className="row w-100">
-        <button onClick={clockMode} className="btn col col-6">
-          Clock
-        </button>
-        <button onClick={timerMode} className="btn col col-6">
-          Timer
-        </button>
-      </div>
-    </nav>
-  </header>
-);
-//true for clock, false for timer
-let mode;
-function clockMode() {
-  mode = true;
-}
-function timerMode() {
-  mode = false;
-}
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>{header}</React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("box")
 );
 
